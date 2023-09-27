269974a00a6797ae41c087ba1af7447ca67ad133
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-27 20:25:20 +0800

    Add path constant

--- constants/index.js
+++ constants/index.js
@@ -2,10 +2,12 @@ const RESOURCE = require("./resource");
 const STATUSCODE = require("./statusCode");
 const METHOD = require("./method");
 const ERROR = require("./error");
+const PATH = require("./path");
 
 module.exports = {
   RESOURCE,
   STATUSCODE,
   METHOD,
+  PATH,
   ERROR,
 };
---
+++ constants/path.js
@@ -0,0 +1,5 @@
+module.exports = {
+  TESTS: "/tests",
+  TEST_ID: "/test/:id",
+  EDIT_TEST_ID: "/test/edit/:id",
+};
