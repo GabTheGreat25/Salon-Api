e691ef783212f0fbe254b3b724498a5bd5963b4c
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-09-27 20:25:04 +0800

    Update server js

--- server.js
+++ server.js
@@ -2,26 +2,27 @@ require("dotenv").config({ path: "./config/.env" });
 const express = require("express");
 const app = express();
 const path = require("path");
-// const cookieParser = require("cookie-parser");
-// const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
-// const mongoose = require("mongoose");
-// const { logger, logEvents } = require("./middleware/logger");
+const mongoose = require("mongoose");
+const { logger, logEvents } = require("./middleware/logger");
 const { errorJson, errorHandler } = require("./middleware/errorJson");
+const test = require("./routes/test");
 const { STATUSCODE } = require("./constants/index");
-// const connectDB = require("./config/ConnectDB");
+const connectDB = require("./config/ConnectDB");
 const PORT = process.env.PORT || 4000;
 
-// connectDB();
-// app.use(logger);
-// app.use(cors(corsOptions));
+connectDB();
+app.use(logger);
+app.use(cors(corsOptions));
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/", require("./routes/root"));
 
-// app.use("/api/v1", auth, users, notes, cameras, transactions, comments);
+app.use("/api/v1", test);
 
 app.all("*", (req, res) => {
   const filePath = req.accepts("html")
@@ -36,13 +37,18 @@ app.all("*", (req, res) => {
 app.use(errorJson);
 app.use(errorHandler);
 
-// mongoose.connection.once("open", () => {
-app.listen(PORT);
-// });
+mongoose.connection.once("open", () => {
+  app.listen(PORT);
+  console.log(`Connected to MongoDB on ${mongoose.connection.host}:${PORT}`);
+});
 
-// mongoose.connection.on("error", (err) => {
-//   logEvents(
-//     `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-//     "mongoErrLog.log"
-//   );
-// });
+mongoose.connection.on("error", (err) => {
+  console.log(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
