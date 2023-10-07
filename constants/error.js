db7301b4f97c475432a5e5be97d9bc0f22da0745
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-10-07 21:49:09 +0800

    Add Too many attempts

--- constants/error.js
+++ constants/error.js
@@ -1,3 +1,5 @@
 module.exports = {
   INTERNAL_SERVER_ERROR: "Internal Server Error",
+  TOO_MANY_ATTEMPTS:
+    "Too many login attempts from this IP, please try again after a 60 second pause",
 };
