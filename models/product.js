64664a456371ac00f088802e48f2723b099d066d
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-10-08 10:42:42 +0800

    Refactor product model

--- models/product.js
+++ models/product.js
@@ -1,38 +1,43 @@
-const mongoose = require("mongoose")
-const {RESOURCE} = require("../constants/index")
+const mongoose = require("mongoose");
+const { RESOURCE } = require("../constants/index");
 
 const productSchema = new mongoose.Schema({
-
-    product_name:{
-
-        type:String,
-        required:[true, "Product Name Field Required"],
-        maxLength:[60, "Product Name Field must not exceed to 60 characters"]
+  product_name: {
+    type: String,
+    required: [true, "Product Name Field Required"],
+    maxLength: [60, "Product Name Field must not exceed to 60 characters"],
+  },
+  brand: {
+    type: String,
+    required: [true, "Product Brand Field Required"],
+    maxLength: [60, "Brand Field must not exceed to 60 characters"],
+  },
+  type: {
+    type: String,
+    required: [true, "Product Type Field Required"],
+    maxLength: [60, "Type Field character must not exceed to 60 characters"],
+  },
+  quantity: {
+    type: Number,
+    required: [true, "Product Quantity Field Required"],
+    maxLength: [8, "Quantity must not exceed to 8 characters"],
+  },
+  image: [
+    {
+      public_id: {
+        type: String,
+        required: true,
+      },
+      url: {
+        type: String,
+        required: true,
+      },
+      originalname: {
+        type: String,
+        required: true,
+      },
     },
+  ],
+});
 
-    brand:{
-        type:String,
-        required:[true, "Product Brand Field Required"],
-        maxLength:[60, "Brand Field must not exceed to 60 characters"]
-    },
-
-    type:{
-        type:String,
-        required:[true, "Product Type Field Required"],
-        maxLength:[60, "Type Field character must not exceed to 60 characters"]
-    },
-
-    quantity:{
-        type:Number,
-        required:[true, "Product Quantity Field Required"],
-        maxLength:[8, "Quantity must not exceed to 8 characters"]
-    },
-
-    createdAt:{
-        type:Date,
-        default:Date.now
-    }
-
-})
-
-module.exports = mongoose.model(RESOURCE.PRODUCT, productSchema)
+module.exports = mongoose.model(RESOURCE.PRODUCT, productSchema);
