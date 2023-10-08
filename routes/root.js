65e0c644dc489c8b05f2edd3045d4379b9db6690
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-10-08 22:35:53 +0800

    Fix spacing

--- routes/root.js
+++ routes/root.js
@@ -3,22 +3,13 @@ const router = express.Router();
 const path = require("path");
 
 router.get(/^\/(?:$|index\.html?$)/, (req, res) => {
-
   const filePath = path.join(__dirname, "..", "views", "index.html");
   res.sendFile(filePath);
-
-  
 });
 
-
-
-router.get(/^\/(?:$|logo\.jpg?$)/, (req, res)=>{
-
-  const imagePath = path.join(__dirname, "..", "views", "logo.jpg")
-  res.sendFile(imagePath)
-
-})
-
-
+router.get(/^\/(?:$|logo\.jpg?$)/, (req, res) => {
+  const imagePath = path.join(__dirname, "..", "views", "logo.jpg");
+  res.sendFile(imagePath);
+});
 
 module.exports = router;
