20a922cd472ebcaf0d9c5d95387e2d502a57fc17
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-10-24 00:28:17 +0800

    Refactor comment model

--- models/comment.js
+++ models/comment.js
@@ -1,21 +1,55 @@
 const mongoose = require("mongoose");
 const { RESOURCE } = require("../constants/index");
+const badWords = require("bad-words");
+const customBadWords = require("../helpers/customBadWords");
 
-const newCommentSchema = new mongoose.Schema({
-    description:{
-        type:String,
-        required:[true, "Description field required"],
+const filter = new badWords();
+filter.addWords(...customBadWords);
+
+const commentSchema = new mongoose.Schema(
+  {
+    transaction: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: RESOURCE.TRANSACTION,
     },
-    suggestion:{
-        type:String,
-        required:[true, "Suggestion field required"],
+    ratings: {
+      type: Number,
+      required: [true, "Please enter a rating"],
+      min: 1,
+      max: 5,
+      validate: {
+        validator: Number.isInteger,
+        message: "Ratings must be a whole number between 1 and 5",
+      },
     },
-    transaction:{
-        type:mongoose.Types.ObjectId,
-        required:[true, "Please enter a transaction"],
-        ref:RESOURCE.TRANSACTION
-    }
+    text: {
+      type: String,
+      required: [true, "Please enter a text"],
+      validate: {
+        validator: function (value) {
+          return !filter.isProfane(value);
+        },
+        message: "Comments cannot contain profanity.",
+      },
+    },
+    image: [
+      {
+        public_id: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+        originalname: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
+  },
+);
 
-});
-
-module.exports = mongoose.model(RESOURCE.COMMENT, newCommentSchema)
+module.exports = mongoose.model(RESOURCE.COMMENT, commentSchema);
