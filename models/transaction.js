894f21772e84af6568a785896384c96087376b23
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-10-23 15:03:39 +0800

    Refactor transaction model

--- models/transaction.js
+++ models/transaction.js
@@ -1,38 +1,40 @@
 const mongoose = require("mongoose");
-const { RESOURCE } = require("../constants/index");
+const {
+    RESOURCE
+} = require("../constants/index");
 
 const newTransactionSchema = new mongoose.Schema({
-    date:{
-        type:Date,
-        required:[true, "Please enter a date"]
+    date: {
+        type: Date,
+        required: [true, "Please enter a date"]
     },
-    time:{
+    time: {
         type: String,
         required: [true, "Please enter a time"],
         validate: {
-        validator: (value) => {
-        return /^[0-2][0-9]:[0-5][0-9]:[0-5][0-9]$/.test(value);
-      },
-      message: "Invalid time format. Please use 'HH:MM:SS'.",
+            validator: (value) => {
+                return /^[0-2][0-9]:[0-5][0-9]:[0-5][0-9]$/.test(value);
+            },
+            message: "Invalid time format. Please use 'HH:MM:SS'.",
+        },
     },
-    },
-    status:{
-        type:String,
-        default:"pending",
-        enum:{
-            values: ["pending","completed","cancel"]
+    status: {
+        type: String,
+        default: "pending",
+        enum: {
+            values: ["pending", "completed", "cancel"]
         }
     },
-    payment:{
-        type:String,
-        required:[true, "Payment field required"],
-        maxLength:[60, "Payment field must not exceed to 60 characters"]
+    payment: {
+        type: String,
+        required: [true, "Payment field required"],
+        maxLength: [60, "Payment field must not exceed to 60 characters"]
     },
-    customer:{
-        type:mongoose.Types.ObjectId,
-        required:[true, "Please enter a customer"],
+    customer: {
+        type: mongoose.Types.ObjectId,
+        required: [true, "Please enter a customer"],
         ref: RESOURCE.USER
     }
 });
 
-module.exports = mongoose.model(RESOURCE.TRANSACTION,  newTransactionSchema);
+module.exports = mongoose.model(RESOURCE.TRANSACTION, newTransactionSchema);
