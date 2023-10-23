e273521bda7d0a6cfe4909baa5cc8faab48cb83e
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-10-24 00:28:48 +0800

    Refactor comment routes

--- routes/comment.js
+++ routes/comment.js
@@ -1,39 +1,47 @@
 const express = require("express");
 const router = express.Router();
 const commentController = require("../controllers/commentController");
-const { METHOD, PATH } = require("../constants/index");
+const { verifyJWT, authorizeRoles } = require("../middleware/verifyJWT");
+const { METHOD, PATH, ROLE } = require("../constants/index");
 
-const testRoutes = [
+router.use(verifyJWT);
+
+const commentRoutes = [
   {
     method: METHOD.GET,
-    path: PATH.COMMENT,
-    handler: commentController.getAllComment
+    path: PATH.COMMENTS,
+    roles: [ROLE.ADMIN, ROLE.EMPLOYEE, ROLE.CUSTOMER],
+    handler: commentController.getAllComments,
+  },
+  {
+    method: METHOD.POST,
+    path: PATH.COMMENTS,
+    roles: [ROLE.ADMIN ,ROLE.CUSTOMER],
+    handler: commentController.createNewComment,
   },
   {
     method: METHOD.GET,
     path: PATH.COMMENT_ID,
-    handler: commentController.getSingleComment
-  },
-  {
-    method: METHOD.POST,
-    path: PATH.COMMENT,
-    handler: commentController.createNewComment
+    roles: [ROLE.ADMIN, ROLE.EMPLOYEE, ROLE.CUSTOMER],
+    handler: commentController.getSingleComment,
   },
   {
     method: METHOD.PATCH,
     path: PATH.EDIT_COMMENT_ID,
+    roles: [ROLE.ADMIN, ROLE.CUSTOMER],
     handler: commentController.updateComment,
   },
   {
     method: METHOD.DELETE,
     path: PATH.COMMENT_ID,
+    roles: [ROLE.ADMIN, ROLE.EMPLOYEE, ROLE.CUSTOMER],
     handler: commentController.deleteComment,
   },
 ];
 
-testRoutes.forEach((route) => {
-  const { method, path, handler } = route;
-  router[method](path, handler);
+commentRoutes.forEach((route) => {
+  const { method, path, roles, handler } = route;
+  router[method](path, authorizeRoles(...roles), handler);
 });
 
 module.exports = router;
