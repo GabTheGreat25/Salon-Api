33a528d0a199283f343357d7796eef21cc559d93
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-02 00:00:59 +0800

    Add middleware and roles logic in test route

--- routes/test.js
+++ routes/test.js
@@ -1,39 +1,62 @@
 const express = require("express");
 const router = express.Router();
 const testController = require("../controllers/testController");
-const { METHOD, PATH } = require("../constants/index");
+const {
+  verifyJWT,
+  authorizeRoles
+} = require("../middleware/verifyJWT");
+const {
+  METHOD,
+  PATH,
+  ROLE
+} = require("../constants/index");
 
-const testRoutes = [
-  {
+const testRoutes = [{
     method: METHOD.GET,
     path: PATH.TESTS,
+    roles: [],
+    middleware: [],
     handler: testController.getAllTests,
   },
   {
     method: METHOD.GET,
     path: PATH.TEST_ID,
+    roles: [],
+    middleware: [],
     handler: testController.getSingleTest,
   },
   {
     method: METHOD.POST,
     path: PATH.TESTS,
+    roles: [ROLE.ADMIN],
+    middleware: [verifyJWT],
     handler: testController.createNewTest,
   },
   {
     method: METHOD.PATCH,
     path: PATH.EDIT_TEST_ID,
+    roles: [],
+    middleware: [],
     handler: testController.updateTest,
   },
   {
     method: METHOD.DELETE,
     path: PATH.TEST_ID,
+    roles: [],
+    middleware: [],
     handler: testController.deleteTest,
   },
 ];
 
 testRoutes.forEach((route) => {
-  const { method, path, handler } = route;
-  router[method](path, handler);
+  const {
+    method,
+    path,
+    roles = [],
+    middleware = [],
+    handler
+  } = route;
+  router[method](path, middleware.concat(authorizeRoles(...roles)), handler);
 });
 
 module.exports = router;
