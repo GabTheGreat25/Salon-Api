2997f915d99a37ada98da7034c203a2a21af1c46
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-03 00:24:17 +0800

    Add verification model

---
+++ models/verification.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const {
+    RESOURCE
+} = require("../constants/index");
+
+const verificationSchema = new mongoose.Schema({
+    transaction: {
+        type: mongoose.Types.ObjectId,
+        required: [true, "Please enter a transaction"],
+        ref: RESOURCE.TRANSACTION
+    },
+    confirm: {
+        type: Boolean,
+        default: false,
+    },
+})
+
+module.exports = mongoose.model(RESOURCE.VERIFICATION, verificationSchema)
