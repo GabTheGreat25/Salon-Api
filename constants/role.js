dcff912821de94b47a44d1157a68ed4d3ae529da
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-02 23:28:42 +0800

    Fix typo

--- constants/role.js
+++ constants/role.js
@@ -2,5 +2,5 @@ module.exports = {
   ADMIN: "Admin",
   EMPLOYEE: "Employee",
   ONLINE_CUSTOMER: "Online Customer",
-  WALK_IN_CUSTOER: "Walk-in Customer",
+  WALK_IN_CUSTOMER: "Walk-in Customer",
 };
