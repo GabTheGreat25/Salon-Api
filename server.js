0068e76881466d68ecdaeb662e191a8f4dd94364
GabTheGreat25 <gabrielarafol.mendoza@tup.edu.ph>
2023-11-10 21:14:55 +0800

    Add compression in server js

--- server.js
+++ server.js
@@ -1,29 +1,41 @@
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config({
+  path: "./config/.env"
+});
 const express = require("express");
+const compression = require("compression");
 const app = express();
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = require("./config/corsOptions");
 const mongoose = require("mongoose");
-const { logger, logEvents } = require("./middleware/logger");
-const { errorJson, errorHandler } = require("./middleware/errorJson");
+const {
+  logger,
+  logEvents
+} = require("./middleware/logger");
+const {
+  errorJson,
+  errorHandler
+} = require("./middleware/errorJson");
 const test = require("./routes/test");
 const products = require("./routes/product");
 const auth = require("./routes/auth");
 const users = require("./routes/user");
-const services = require("./routes/service")
+const services = require("./routes/service");
 const schedules = require("./routes/schedule");
 const delivery = require("./routes/delivery");
 const comments = require("./routes/comment");
 const appointments = require("./routes/appointment");
 const transactions = require("./routes/transaction");
-const { STATUSCODE } = require("./constants/index");
+const {
+  STATUSCODE
+} = require("./constants/index");
 const connectDB = require("./config/connectDB");
 const PORT = process.env.PORT || 4000;
 
 connectDB();
 app.use(logger);
+app.use(compression());
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
@@ -31,14 +43,28 @@ app.use(cookieParser());
 app.use("/", express.static(path.join(__dirname, "/public")));
 app.use("/", require("./routes/root"));
 
-app.use("/api/v1", test, auth, users, products, schedules, services, delivery, appointments, comments, transactions);
+app.use(
+  "/api/v1",
+  test,
+  auth,
+  users,
+  products,
+  schedules,
+  services,
+  delivery,
+  appointments,
+  comments,
+  transactions
+);
 
 app.all("*", (req, res) => {
-  const filePath = req.accepts("html")
-    ? path.join(__dirname, "views", "404.html")
-    : req.accepts("json")
-    ? { message: "404 Not Found" }
-    : "404 Not Found";
+  const filePath = req.accepts("html") ?
+    path.join(__dirname, "views", "404.html") :
+    req.accepts("json") ?
+    {
+      message: "404 Not Found"
+    } :
+    "404 Not Found";
 
   res.status(STATUSCODE.NOT_FOUND).sendFile(filePath);
 });
