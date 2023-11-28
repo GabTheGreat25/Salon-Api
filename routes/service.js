678ec03aa2c58008c83830c8b2509b39211bec46
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-11-28 14:51:18 +0800

    Fix routes of service to handle roles and authentication

--- routes/service.js
+++ routes/service.js
@@ -1,39 +1,57 @@
 const express = require("express");
 const router = express.Router();
 const serviceController = require("../controllers/serviceController");
-const { METHOD, PATH } = require("../constants/index");
+const { verifyJWT, authorizeRoles } = require("../middleware/verifyJWT");
+const { METHOD, PATH, ROLE } = require("../constants/index");
+
+router.use(verifyJWT);
 
 const serviceRoutes = [
   {
     method: METHOD.GET,
     path: PATH.SERVICE,
+    roles: [
+      ROLE.ADMIN,
+      ROLE.EMPLOYEE,
+      ROLE.ONLINE_CUSTOMER,
+      ROLE.WALK_IN_CUSTOMER,
+    ],
     handler: serviceController.getAllServices,
   },
   {
     method: METHOD.GET,
     path: PATH.SERVICE_ID,
+    roles: [
+      ROLE.ADMIN,
+      ROLE.EMPLOYEE,
+      ROLE.ONLINE_CUSTOMER,
+      ROLE.WALK_IN_CUSTOMER,
+    ],
     handler: serviceController.getSingleService,
   },
   {
     method: METHOD.POST,
     path: PATH.SERVICE,
+    roles: [ROLE.ADMIN],
     handler: serviceController.createNewService,
   },
   {
     method: METHOD.PATCH,
     path: PATH.EDIT_SERVICE_ID,
+    roles: [ROLE.ADMIN],
     handler: serviceController.updateService,
   },
   {
     method: METHOD.DELETE,
     path: PATH.SERVICE_ID,
+    roles: [ROLE.ADMIN],
     handler: serviceController.deleteService,
   },
 ];
 
 serviceRoutes.forEach((route) => {
-  const { method, path, handler } = route;
-  router[method](path, handler);
+  const { method, path, roles, handler } = route;
+  router[method](path, authorizeRoles(...roles), handler);
 });
 
 module.exports = router;
