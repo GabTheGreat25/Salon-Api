c2907c02e1d69a1bab12243c543dd8b7dae456b1
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-04 13:26:46 +0800

    Refactor transaction model

--- models/transaction.js
+++ models/transaction.js
@@ -1,44 +1,30 @@
 const mongoose = require("mongoose");
-const {
-    RESOURCE
-} = require("../constants/index");
+const { RESOURCE } = require("../constants/index");
 
 const transactionSchema = new mongoose.Schema({
-    appointment: {
-        type: mongoose.Types.ObjectId,
-        required: [true, "Please enter a appointment"],
-        ref: RESOURCE.APPOINTMENT,
+  appointment: {
+    type: mongoose.Types.ObjectId,
+    required: [true, "Please enter a appointment"],
+    ref: RESOURCE.APPOINTMENT,
+  },
+  date: {
+    type: Date,
+    required: [true, "Please enter a date"],
+  },
+  status: {
+    type: String,
+    default: "pending",
+    enum: {
+      values: ["pending", "completed", "cancelled"],
     },
-    customer: {
-        type: mongoose.Types.ObjectId,
-        required: [true, "Please enter a user"],
-        ref: RESOURCE.USER,
+  },
+  payment: {
+    type: String,
+    default: "Cash",
+    enum: {
+      values: ["Cash", "Gcash"],
     },
-    date: {
-        type: Date,
-        required: [true, "Please enter a date"]
-    },
-    time: {
-        type: String,
-        required: [true, "Please enter a time"],
-        validate: {
-            validator: (value) => {
-                return /^[0-2][0-9]:[0-5][0-9]:[0-5][0-9]$/.test(value);
-            },
-            message: "Invalid time format. Please use 'HH:MM:SS'.",
-        },
-    },
-    status: {
-        type: String,
-        default: "pending",
-        enum: {
-            values: ["pending", "completed", "cancelled"]
-        }
-    },
-    payment: {
-        type: String,
-        required: [true, "Payment field required"],
-    },
+  },
 });
 
 module.exports = mongoose.model(RESOURCE.TRANSACTION, transactionSchema);
