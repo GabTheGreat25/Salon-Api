3a76687b411d588f90e8d1d20be96d4de1759ab9
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-12 21:14:42 +0800

    Add feedback model

---
+++ models/feedback.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const { RESOURCE } = require("../constants/index");
+const validator = require("validator");
+
+const feedbackSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name Field Required"],
+    maxLength: [60, "Name Field must not exceed to 60 characters"],
+  },
+  email: {
+    type: String,
+    required: [true, "Feedback Email Field Required"],
+    unique: true,
+    validate: [validator.isEmail, "Please enter valid email address"],
+  },
+  contact_number: {
+    type: String,
+    required: [true, "Contact number Field Required"],
+    unique: true,
+    validate: [
+      {
+        validator: function (value) {
+          return /^\d{11}$/.test(value);
+        },
+        message: "Contact number must be exactly 11 digits.",
+      },
+      {
+        validator: function (value) {
+          return /^09\d{9}$/.test(value);
+        },
+        message: "Invalid Philippine contact number format.",
+      },
+    ],
+  },
+  description: {
+    type: String,
+    required: [true, "Description Field Required"],
+    maxLength: [60, "Description Field must not exceed to 60 characters"],
+  },
+});
+
+module.exports = mongoose.model(RESOURCE.FEEDBACK, feedbackSchema);
