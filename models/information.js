8fb73ca3ca8c93bad800c141a24ce0e51fa1b4f7
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2023-12-12 15:42:54 +0800

    Make the description, allergies, and product_preference to be required

--- models/information.js
+++ models/information.js
@@ -1,26 +1,24 @@
 const mongoose = require("mongoose");
-const {
-    RESOURCE
-} = require("../constants/index");
+const { RESOURCE } = require("../constants/index");
 
 const informationSchema = new mongoose.Schema({
-    customer: {
-        type: mongoose.Types.ObjectId,
-        required: [true, "Please enter a customer"],
-        ref: RESOURCE.USER
-    },
-    description: {
-        type: String,
-        required: false,
-    },
-    allergy: {
-        type: String,
-        required: false,
-    },
-    product_preference: {
-        type: String,
-        required: false,
-    },
-})
+  customer: {
+    type: mongoose.Types.ObjectId,
+    required: [true, "Please enter a customer"],
+    ref: RESOURCE.USER,
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter your description"],
+  },
+  allergy: {
+    type: String,
+    required: [true, "Please enter your allergy"],
+  },
+  product_preference: {
+    type: String,
+    required: [true, "Please enter your product preference"],
+  },
+});
 
-module.exports = mongoose.model(RESOURCE.INFORMATION, informationSchema)
+module.exports = mongoose.model(RESOURCE.INFORMATION, informationSchema);
