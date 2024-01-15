bdfa6ed9aea721263c93165870a4567471da72cb
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-15 11:40:01 +0800

    Add twilio sms logic

---
+++ utils/twilio.js
@@ -0,0 +1,20 @@
+const twilio = require("twilio");
+
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const client = twilio(accountSid, authToken);
+
+const sendSMS = async (to, message) => {
+  try {
+    await client.messages.create({
+      body: message,
+      from: process.env.TWILIO_PHONE_NUMBER,
+      to: to,
+    });
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
+
+module.exports = { sendSMS };
