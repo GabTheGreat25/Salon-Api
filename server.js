fcdb1af566289fc78a561949adbb979c143b50d5
Jay200123 <kenken.ayonon@gmail.com>
2024-01-21 20:51:37 +0800

    Add brands route

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const comments = require("./routes/comment");
 const appointments = require("./routes/appointment");
 const transactions = require("./routes/transaction");
 const feedbacks = require("./routes/feedback");
+const brands = require("./routes/brand");
 const { STATUSCODE } = require("./constants/index");
 const connectDB = require("./config/connectDB");
 const PORT = process.env.PORT || 4000;
@@ -40,6 +41,7 @@ app.use(
   "/api/v1",
   test,
   feedbacks,
+  brands, 
   auth,
   users,
   products,
@@ -48,7 +50,7 @@ app.use(
   delivery,
   appointments,
   comments,
-  transactions
+  transactions,
 );
 
 app.all("*", (req, res) => {
