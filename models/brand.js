5b41c68e6a32be40290addb443ae633d60ba48c6
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-01-22 23:43:05 +0800

    Fix spacing

--- models/brand.js
+++ models/brand.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const { RESOURCE } = require("../constants/index");
 
 const brandSchema = new mongoose.Schema({
-    brand_name:{
-        type:String,
-        required:[true, "brand name field required"]
-    }
+  brand_name: {
+    type: String,
+    required: [true, "brand name field required"],
+  },
 });
 
 module.exports = mongoose.model(RESOURCE.BRAND, brandSchema);
