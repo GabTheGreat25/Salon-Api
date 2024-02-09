ec0135f29088ad17195928af400ae255e39a1ea6
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-02-10 01:30:30 +0800

    Add addOns model

---
+++ models/addOns.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const { RESOURCE } = require("../constants/index");
+
+const addOnsSchema = new mongoose.Schema({
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "Please enter a product"],
+    ref: "product",
+  },
+  addOns_name: {
+    type: String,
+    required: [true, "AddOns name required"],
+    maxLength: [60, "AddOns Name Field must not exceed to 60 characters"],
+  },
+  description: {
+    type: String,
+    required: [true, "Please enter a description of your addOns"],
+  },
+  price: {
+    type: Number,
+    required: [true, "Please enter a price"],
+    min: 0,
+  },
+});
+
+module.exports = mongoose.model(RESOURCE.ADDONS, addOnsSchema);
