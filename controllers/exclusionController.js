cc05e94413ce2ba6fb8ddd3b03856ae887bfab5e
Jay200123 <kenken.ayonon@gmail.com>
2024-02-13 21:00:27 +0800

    Add exclusionController

---
+++ controllers/exclusionController.js
@@ -0,0 +1,76 @@
+const SuccessHandler = require("../utils/successHandler");
+const ErrorHandler = require("../utils/errorHandler");
+const exclusionServices = require("../services/exclusionService");
+const asyncHandler = require("express-async-handler");
+const checkRequiredFields = require("../helpers/checkRequiredFields");
+const { STATUSCODE } = require("../constants/index");
+
+exports.getAllExclusions = asyncHandler(async (req, res, next) => {
+  const exclusions = await exclusionServices.getAllExclusionData();
+
+  return exclusions?.length === STATUSCODE.ZERO
+    ? next(new ErrorHandler("No exclusions found"))
+    : SuccessHandler(
+        res,
+        `Exclusions with ingredients ${exclusions
+          .map((e) => e.ingredient_name)
+          .join(", ")} and IDs ${exclusions.map((e) => e._id).join(", ")} retrieved`,
+        exclusions
+      );
+});
+
+exports.getSingleExclusion = asyncHandler(async (req, res, next) => {
+  const exclusion = await exclusionServices.getSingleExclusionData(
+    req.params?.id
+  );
+
+  return !exclusion
+    ? next(new ErrorHandler("No Ingredients found"))
+    : SuccessHandler(
+        res,
+        `Test ${exclusion?.ingredient_name} with ID ${exclusion?._id} retrieved`,
+        exclusion
+      );
+});
+
+exports.createNewExclusion = [
+  checkRequiredFields(["ingredient_name", "type"]),
+  asyncHandler(async (req, res, next) => {
+    const exclusion = await exclusionServices.createExclusionData(req);
+
+    return SuccessHandler(
+      res,
+      `Created new Exclusion ${exclusion?.ingredient_name} with an ID ${exclusion?._id}`,
+      exclusion
+    );
+  }),
+];
+
+exports.updateExclusion = [
+  checkRequiredFields(["ingredient_name", "type"]),
+  asyncHandler(async (req, res, next) => {
+    const exclusion = await exclusionServices.updateExclusionData(
+      req,
+      res,
+      req.params.id
+    );
+
+    return SuccessHandler(
+      res,
+      `Ingredient ${exclusion?.ingredient_name} with ID ${exclusion?._id} is updated`,
+      exclusion
+    );
+  }),
+];
+
+exports.deleteExclusion = asyncHandler(async (req, res, next) => {
+  const exclusion = await exclusionServices.deleteExclusionData(req.params.id);
+
+  return !exclusion
+    ? next(new ErrorHandler("No ingredient found"))
+    : SuccessHandler(
+        res,
+        `Ingredients ${exclusion?.ingredient_name} with ID ${exclusion?._id} is deleted`,
+        exclusion
+      );
+});
