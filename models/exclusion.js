ad4d3ee6855af071ecb90837b701f4ed0bbd5f04
Jay200123 <kenken.ayonon@gmail.com>
2024-02-13 21:00:06 +0800

    Add exclusion Model

---
+++ models/exclusion.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+const { RESOURCE } = require("../constants/index");
+
+const exclusionSchema = new mongoose.Schema({
+    ingredient_name:{
+        type: String,
+        required: [true, "Ingredient Name field required"],
+        maxLength: [255,"Ingredients name must not exceed to 255 characters"],
+    },
+    type:{
+        type: String,
+        enum: ["Hands", "Hair", "Feet", "Face", "Body"],
+    }
+});
+
+module.exports = mongoose.model(RESOURCE.EXCLUSION, exclusionSchema);
