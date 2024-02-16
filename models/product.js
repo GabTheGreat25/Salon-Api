8d4926c943feea6669ee6094aad9b32c401fa7a5
Jay200123 <kenken.ayonon@gmail.com>
2024-02-16 19:01:42 +0800

    Add Nails in type field for product model

--- models/product.js
+++ models/product.js
@@ -14,7 +14,7 @@ const productSchema = new mongoose.Schema({
   },
   type: {
     type: String,
-    enum: ["Hands", "Hair", "Feet", "Face", "Body"],
+    enum: ["Hands", "Hair", "Feet", "Face", "Body", "Nails"],
   },
   // measurement: {
   //   quantity: {
