64b38e4c4acfdb0d483139d88759fad54b273961
Jay200123 <kenken.ayonon@gmail.com>
2024-03-03 20:16:10 +0800

    Add facial to delivery type

--- models/delivery.js
+++ models/delivery.js
@@ -38,7 +38,7 @@ const deliverySchema = new mongoose.Schema({
   type: [
     {
       type: String,
-      enum: ["Hands", "Hair", "Feet", "Face", "Body", "Eyelash"],
+      enum: ["Hands", "Hair", "Feet", "Facial", "Body", "Eyelash"],
     },
   ],
 });
