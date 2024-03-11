83c6f44c5a4c36c31476704fe51666d6d1ba5786
Jay200123 <kenken.ayonon@gmail.com>
2024-03-11 20:57:36 +0800

    Add equipment controller

---
+++ controllers/equipmentController.js
@@ -0,0 +1,95 @@
+const SuccessHandler = require("../utils/successHandler");
+const ErrorHandler = require("../utils/errorHandler");
+const equipmentServices = require("../services/equipmentService");
+const asyncHandler = require("express-async-handler");
+const checkRequiredFields = require("../helpers/checkRequiredFields");
+const { upload } = require("../utils/cloudinary");
+const { STATUSCODE } = require("../constants/index");
+
+exports.getAllEquipments = asyncHandler(async (req, res, next) => {
+  const equipments = await equipmentServices.getAllEquipmentData();
+
+  return equipments?.length === STATUSCODE.ZERO
+    ? next(new ErrorHandler("No equipments found"))
+    : SuccessHandler(
+        res,
+        `Equipments with equipment name ${equipments
+          .map((e) => e.equipment_name)
+          .join(", ")} and IDs ${equipments
+          .map((u) => u._id)
+          .join(", ")} retrieved`,
+        equipments
+      );
+});
+
+exports.getSingleEquipment = asyncHandler(async (req, res, next) => {
+  const equipment = await equipmentServices.getSingleEquipmentData(
+    req.params?.id
+  );
+
+  return !equipment
+    ? next(new ErrorHandler("No equipment found"))
+    : SuccessHandler(
+        res,
+        `Equipment ${equipment?.equipment_name} with ID ${equipment?._id} retrieved`,
+        equipment
+      );
+});
+
+exports.createNewEquipment = [
+  upload.array("image"),
+  checkRequiredFields([
+    "equipment_name",
+    "description",
+    "equipment_price",
+    "purchased_date",
+    "status",
+    "image",
+  ]),
+  asyncHandler(async (req, res, next) => {
+    const equipment = await equipmentServices.createEquipmentData(req);
+
+    return SuccessHandler(
+      res,
+      `Created new Equipment ${equipment?.equipment_name} with an ID ${equipment?._id}`,
+      equipment
+    );
+  }),
+];
+
+exports.updateEquipment = [
+  upload.array("image"),
+  checkRequiredFields([
+    "equipment_name",
+    "description",
+    "equipment_price",
+    "purchased_date",
+    "status",
+    "image",
+  ]),
+  asyncHandler(async (req, res, next) => {
+    const equipment = await equipmentServices.updateEquipmentData(
+      req,
+      res,
+      req.params.id
+    );
+
+    return SuccessHandler(
+      res,
+      `Equipment ${equipment?.equipment_name} with ID ${equipment?._id} is updated`,
+      equipment
+    );
+  }),
+];
+
+exports.deleteEquipment = asyncHandler(async (req, res, next) => {
+  const equipment = await equipmentServices.deleteEquipmentData(req.params.id);
+
+  return !equipment
+    ? next(new ErrorHandler("No equipment found"))
+    : SuccessHandler(
+        res,
+        `Equipment ${equipment?.equipment_name} with ID ${equipment?._id} is deleted`,
+        equipment
+      );
+});
