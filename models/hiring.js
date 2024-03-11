1a2e5cc761d2c190bf807b216e58fc9b7cf117f9
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-11 23:55:20 +0800

    Remove time validation and enum for hiring type

--- models/hiring.js
+++ models/hiring.js
@@ -7,16 +7,9 @@ const hiringSchema = new mongoose.Schema({
   },
   time: {
     type: String,
-    validate: {
-      validator: (value) => {
-        return /^(0?[1-9]|1[0-2]):[0-5][0-9] (AM|PM)$/i.test(value);
-      },
-      message: "Invalid time format. Please use 'HH:MM AM/PM'.",
-    },
   },
   type: {
     type: String,
-    enum: ["Beautician", "Receptionist"],
   },
   isHiring: {
     type: Boolean,
