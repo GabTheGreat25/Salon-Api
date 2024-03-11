67a8eb0466d32403cf443b81edf183fa25326580
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-03-11 23:55:30 +0800

    Remove verifyJWT middleware from hiring routes

--- routes/hiring.js
+++ routes/hiring.js
@@ -1,45 +1,48 @@
 const express = require("express");
 const router = express.Router();
 const hiringController = require("../controllers/hiringController");
-const { verifyJWT, authorizeRoles } = require("../middleware/verifyJWT");
+const { authorizeRoles } = require("../middleware/verifyJWT");
 const { METHOD, PATH, ROLE } = require("../constants/index");
 
-router.use(verifyJWT);
-
-const testRoutes = [
+const hiringRoutes = [
   {
     method: METHOD.GET,
     path: PATH.HIRINGS,
-    roles: [ROLE.ADMIN],
+    roles: [],
+    middleware: [],
     handler: hiringController.getAllHiring,
   },
   {
     method: METHOD.GET,
     path: PATH.HIRING_ID,
-    roles: [ROLE.ADMIN],
+    roles: [],
+    middleware: [],
     handler: hiringController.getSingleHiring,
   },
   {
     method: METHOD.POST,
     path: PATH.HIRINGS,
-    roles: [ROLE.ADMIN],
+    roles: [],
+    middleware: [],
     handler: hiringController.createNewHiring,
   },
   {
     method: METHOD.PATCH,
     path: PATH.EDIT_HIRING_ID,
-    roles: [ROLE.ADMIN],
+    roles: [],
+    middleware: [],
     handler: hiringController.updateHiring,
   },
   {
     method: METHOD.DELETE,
     path: PATH.HIRING_ID,
-    roles: [ROLE.ADMIN],
+    roles: [],
+    middleware: [],
     handler: hiringController.deleteHiring,
   },
 ];
 
-testRoutes.forEach((route) => {
+hiringRoutes.forEach((route) => {
   const { method, path, roles = [], middleware = [], handler } = route;
   router[method](path, middleware.concat(authorizeRoles(...roles)), handler);
 });
