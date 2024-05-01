4eb5a6661027e2e0c57df447a1be217d8a756027
Jay200123 <kenken.ayonon@gmail.com>
2024-05-02 00:03:03 +0800

    Add logbook model

---
+++ models/logbook.js
@@ -0,0 +1,72 @@
+const mongoose = require("mongoose");
+const { RESOURCE } = require("../constants/index");
+
+const LogbookSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    required: [true, "user required"],
+    ref: RESOURCE.USER,
+  },
+  equipment: [
+    {
+      equipment: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: [true, "equipment required"],
+        ref: RESOURCE.EQUIPMENT,
+      },
+      borrow_quantity: {
+        type: Number,
+        required: [true, "Borrowed Equipment Quantity required"],
+        default: 0,
+      },  
+      missing_quantity: {
+        type: Number,
+        required: false,
+        default: 0,
+      },
+      damage_quantity: {
+        type: Number,
+        required: false,
+        default: 0,
+      },
+      status: {
+        type: String,
+        enum: ["Found", "Missing", "Damage", "Damage & Missing"],
+        default: "Found",
+      },
+    },
+  ],
+  date_borrowed: {
+    type: Date,
+    reqiuired: false,
+    default: Date.now(),
+  },
+  time_borrowed: {
+    type: String,
+    default: function () {
+      const currentTime = new Date();
+      return currentTime.toLocaleTimeString([], { hour12: true });
+    },
+  },
+  date_returned: {
+    type: Date,
+    required: false,
+  },
+  note: {
+    type: String,
+    required: false,
+  },
+  status: {
+    type: String,
+    enum: [
+      "Borrowed",
+      "Returned",
+      "Returned With Missing",
+      "Returned With Damage",
+      "Returned Damage & Missing",
+    ],
+    default: "Borrowed",
+  },
+});
+
+module.exports = mongoose.model(RESOURCE.LOGBOOK, LogbookSchema);
