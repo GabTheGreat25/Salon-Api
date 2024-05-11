b34250945581e79103a5cadaaf406966731a356c
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-05-11 15:04:27 +0800

    Refactor eclusion model

--- models/exclusion.js
+++ models/exclusion.js
@@ -5,7 +5,6 @@ const exclusionSchema = new mongoose.Schema({
   ingredient_name: {
     type: String,
     required: [true, "Ingredient Name field required"],
-    maxLength: [255, "Ingredients name must not exceed to 255 characters"],
   },
   type: [
     {
