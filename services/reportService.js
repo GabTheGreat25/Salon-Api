2c114ecec166b1194f32324d5114e9ea9ecfdf86
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-05-11 15:07:54 +0800

    Refactor reportService

--- services/reportService.js
+++ services/reportService.js
@@ -2,16 +2,15 @@ const Report = require("../models/report");
 const Equipment = require("../models/equipment");
 const ErrorHandler = require("../utils/errorHandler");
 const mongoose = require("mongoose");
-const { RESOURCE } = require("../constants/index");
-const equipment = require("../models/equipment");
+const { RESOURCE, STATUSCODE } = require("../constants/index");
 
 exports.getAllReportsData = async () => {
   const reports = await Report.find()
-  .populate({
-    path: "equipment",
-    select: "equipment_name image"
-  })
-    .sort({ createdAt: -1 })
+    .sort({ createdAt: STATUSCODE.NEGATIVE_ONE })
+    .populate({
+      path: RESOURCE.EQUIPMENT,
+      select: "equipment_name image",
+    })
     .lean()
     .exec();
 
@@ -19,21 +18,18 @@ exports.getAllReportsData = async () => {
 };
 
 exports.getOneReportData = async (id) => {
-  if (!mongoose.Types.ObjectId.isValid(id)) {
+  if (!mongoose.Types.ObjectId.isValid(id))
     throw new ErrorHandler(`Invalid Report ID: ${id}`);
-  }
 
   const report = await Report.findById(id)
     .populate({
-      path: "equipment",
+      path: RESOURCE.EQUIPMENT,
       select: "equipment_name",
     })
     .lean()
     .exec();
 
-  if (!report) {
-    throw new ErrorHandler(`Report not found with ID: ${id}`);
-  }
+  if (!report) throw new ErrorHandler(`Report not found with ID: ${id}`);
 
   return report;
 };
@@ -43,54 +39,48 @@ exports.createReportData = async (req, res) => {
     ...req.body,
   });
 
-  try {
-    const { equipment, quantity_missing, damage_quantity, status } = req.body;
+  const { equipment, quantity_missing, damage_quantity, status } = req.body;
 
-    const lossEquipment = await Equipment.findById(equipment).lean().exec();
+  const lossEquipment = await Equipment.findById(equipment).lean().exec();
 
-    let { _id, quantity, missing_qty, damage_qty } = lossEquipment;
+  let { _id, quantity, missing_qty, damage_qty } = lossEquipment;
 
-    let equipmentQty = 0;
-    let missingQty = 0;
-    let damageQty = 0;
+  let equipmentQty = STATUSCODE.ZERO;
+  let missingQty = STATUSCODE.ZERO;
+  let damageQty = STATUSCODE.ZERO;
 
-    const isMissing = status === "Missing";
-    if (isMissing) {
-      equipmentQty = quantity - quantity_missing;
-      missingQty = missing_qty + quantity_missing;
-    }
-
-    const isDamage = status === "Damage";
-    if (isDamage) {
-      equipmentQty = quantity - damage_quantity;
-      damageQty = damage_qty + damage_quantity;
-    }
-
-    const equipmentStatus = quantity === 0 ? "Not Available" : "Available";
-
-    const updateEquipment = await Equipment.findByIdAndUpdate(_id, {
-      quantity: equipmentQty,
-      missing_qty: missingQty,
-      damage_qty: damageQty,
-      status: equipmentStatus,
-    });
-  } catch (err) {
-    throw new ErrorHandler(err);
+  const isMissing = status === "Missing";
+  if (isMissing) {
+    equipmentQty = quantity - quantity_missing;
+    missingQty = missing_qty + quantity_missing;
   }
+
+  const isDamage = status === "Damage";
+  if (isDamage) {
+    equipmentQty = quantity - damage_quantity;
+    damageQty = damage_qty + damage_quantity;
+  }
+
+  const equipmentStatus =
+    quantity === STATUSCODE.ZERO ? "Not Available" : "Available";
+
+  await Equipment.findByIdAndUpdate(_id, {
+    quantity: equipmentQty,
+    missing_qty: missingQty,
+    damage_qty: damageQty,
+    status: equipmentStatus,
+  });
 
   return report;
 };
 
 exports.updateReportData = async (req, res, id) => {
-  if (!mongoose.Types.ObjectId.isValid(id)) {
+  if (!mongoose.Types.ObjectId.isValid(id))
     throw new ErrorHandler(`Invalid Report ID ${id}`);
-  }
 
   const report = await Report.findById(id).lean().exec();
 
-  if (!report) {
-    throw new ErrorHandler(`Report not Found with ID: ${id}`);
-  }
+  if (!report) throw new ErrorHandler(`Report not Found with ID: ${id}`);
 
   const updateReport = await Report.findByIdAndUpdate(
     id,
@@ -103,89 +93,73 @@ exports.updateReportData = async (req, res, id) => {
     }
   );
 
-  try {
-    let { equipment, status, input_qty, quantity_missing, initial_found } = updateReport;
+  let { equipment, status, input_qty, quantity_missing, initial_found } =
+    updateReport;
 
-    const id = equipment;
+  const id = equipment;
 
-    const reportEquipment = await Equipment.findById(id)
-    .lean()
-    .exec();
+  const reportEquipment = await Equipment.findById(id).lean().exec();
 
-    if (!reportEquipment) {
-      throw new ErrorHandler(`Equipment not Found with ID: ${equipment}`);
-    }
+  if (!reportEquipment)
+    throw new ErrorHandler(`Equipment not Found with ID: ${equipment}`);
 
-    let { quantity, missing_qty, damage_qty, found_qty } = reportEquipment;
+  let { quantity, missing_qty, damage_qty, found_qty } = reportEquipment;
 
-    if (input_qty > missing_qty) {
-      throw new ErrorHandler(
-        "Quantity Found should not exceed Missing Quantity"
-      );
-    }
+  if (input_qty > missing_qty)
+    throw new ErrorHandler("Quantity Found should not exceed Missing Quantity");
 
-    // for equipment
-    let newQuantity = quantity;
-    let newMissingQty = missing_qty;
-    let newDamageQty = damage_qty;
-    let newFoundQty = found_qty;
+  let newQuantity = quantity;
+  let newMissingQty = missing_qty;
+  let newDamageQty = damage_qty;
+  let newFoundQty = found_qty;
 
-    //for report
-    let OldMissing = quantity_missing; //current missing qty ni report;
-    let initialFound = initial_found;
+  let OldMissing = quantity_missing;
+  let initialFound = initial_found;
 
-    const isFound = status === "Found" || "Partially Found";
-    if (isFound) {
-      newQuantity = quantity += input_qty;
-      newMissingQty = missing_qty -= input_qty;
-      newFoundQty = found_qty += input_qty;
-      OldMissing = quantity_missing -= input_qty;
-      initialFound = initial_found += input_qty;
-    };
-
-
-    const isDamage = status === "Found Damage";
-    if(isDamage){
-      newMissingQty = missing_qty -= input_qty;
-      newDamageQty = damage_qty + input_qty;
-    };
-
-    const updateEquipment = await Equipment.findByIdAndUpdate(
-      id,
-      {
-        quantity: newQuantity,
-        missing_qty: newMissingQty,
-        damage_qty: newDamageQty,
-        quantity_found: newFoundQty,
-      },
-      {
-        new: true,
-        runValidators: true,
-      }
-    );
-
-    updateReport.quantity_missing = OldMissing;
-    updateReport.initial_found = initialFound;
-    updateReport.input_qty = 0; 
-    await updateReport.save();
-      
-  } catch (err) {
-    throw new ErrorHandler(err);
+  const isFound = status === "Found" || "Partially Found";
+  if (isFound) {
+    newQuantity = quantity += input_qty;
+    newMissingQty = missing_qty -= input_qty;
+    newFoundQty = found_qty += input_qty;
+    OldMissing = quantity_missing -= input_qty;
+    initialFound = initial_found += input_qty;
   }
+
+  const isDamage = status === "Found Damage";
+  if (isDamage) {
+    newMissingQty = missing_qty -= input_qty;
+    newDamageQty = damage_qty + input_qty;
+  }
+
+  await Equipment.findByIdAndUpdate(
+    id,
+    {
+      quantity: newQuantity,
+      missing_qty: newMissingQty,
+      damage_qty: newDamageQty,
+      quantity_found: newFoundQty,
+    },
+    {
+      new: true,
+      runValidators: true,
+    }
+  );
+
+  updateReport.quantity_missing = OldMissing;
+  updateReport.initial_found = initialFound;
+  updateReport.input_qty = STATUSCODE.ZERO;
+  await updateReport.save();
 
   return updateReport;
 };
 
 exports.deleteReportData = async (id) => {
-  if (!mongoose.Types.ObjectId.isValid(id)) {
+  if (!mongoose.Types.ObjectId.isValid(id))
     throw new ErrorHandler(`Invalid Report ID: ${id}`);
-  }
 
   const report = await Report.findById(id).lean().exec();
 
-  if (!report) {
-    throw new ErrorHandler(`Report not Found with ID: ${id}`);
-  }
+  if (!report) throw new ErrorHandler(`Report not Found with ID: ${id}`);
 
   await Promise.all([
     Report.deleteOne({
