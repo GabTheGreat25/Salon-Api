22360e0988b57ab278f130108ba5f0daa60b1658
GabTheGreat <gabrielarafol.mendoza@tup.edu.ph>
2024-05-11 15:05:06 +0800

    Refactor product model

--- models/product.js
+++ models/product.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
-const { RESOURCE } = require("../constants/index");
+const { RESOURCE, STATUSCODE, ROLE } = require("../constants/index");
 const User = require("./user");
 const { sendSMS } = require("../utils/twilio");
-
 
 const productSchema = new mongoose.Schema({
   product_name: {
     type: String,
     required: [true, "Product Name Field Required"],
-    maxLength: [60, "Product Name Field must not exceed 60 characters"],
   },
   brand: {
     type: mongoose.Schema.Types.ObjectId,
@@ -21,26 +19,6 @@ const productSchema = new mongoose.Schema({
       enum: ["Hands", "Hair", "Feet", "Facial", "Body", "Eyelash"],
     },
   ],
-  // measurement: {
-  //   quantity: {
-  //     type: Number,
-  //     required: [true, "Quantity Field Required"],
-  //     min: [0, "Quantity must be a positive number"],
-  //   },
-  //   unit: {
-  //     type: String,
-  //     required: [true, "Unit Field Required"],
-  //     enum: [
-  //       "Liter",
-  //       "Milliliter",
-  //       "Gallon",
-  //       "Ounce",
-  //       "Pound",
-  //       "Kilogram",
-  //       "Other",
-  //     ],
-  //   },
-  // },
   isNew: {
     type: Boolean,
     default: false,
@@ -68,56 +46,55 @@ const productSchema = new mongoose.Schema({
   quantity: {
     type: Number,
     required: false,
-    default: 0,
+    default: STATUSCODE.ZERO,
   },
   product_volume: {
     type: Number,
-    // required: [true, "Product volume required"],
-    min: 5,
+    min: STATUSCODE.FIVE,
   },
-  product_consume:{
+  product_consume: {
     type: Number,
     required: [true, "Consumed product per service required"],
-    default: 0,
+    default: STATUSCODE.ZERO,
   },
-  remaining_volume:{
+  remaining_volume: {
     type: Number,
-    default: 0,
+    default: STATUSCODE.ZERO,
   },
   product_measurement: {
     type: String,
-    enum: ["ml","liter", "pcs"],
+    enum: ["ml", "liter", "pcs"],
     default: "ml",
   },
   volume_description: {
     type: String,
     enum: ["Milliliter", "Pieces"],
     default: "Milliliter",
-  }
+  },
 });
 
 productSchema.pre("save", async function (next) {
   try {
-    if (this.quantity <= 10) {
-        const getAdminUsers = async () => {
-          const admins = await User.find({ roles: "Admin" });
-          return admins;
-        };
-    
+    if (this.quantity <= STATUSCODE.TEN) {
+      const getAdminUsers = async () => {
+        const admins = await User.find({ roles: ROLE.ADMIN });
+        return admins;
+      };
+
       const admins = await getAdminUsers();
       const adminNames = admins.map((admin) => admin.name);
       const adminNumbers = admins.map((admin) => admin.contact_number);
-    
+
       const smsAdminMessage = `Product ${this.product_name} has ${this.quantity} quantity left`;
       adminNumbers.forEach((number, index) => {
         console.log(`Sending SMS to ${adminNames[index]} at ${number}`);
         console.log(smsAdminMessage);
-        sendSMS(`+63${number.substring(1)}`, smsAdminMessage);
+        sendSMS(`+63${number.substring(STATUSCODE.ONE)}`, smsAdminMessage);
       });
     }
     next();
   } catch (error) {
-    next(error); 
+    next(error);
   }
 });
 
