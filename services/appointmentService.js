f27f9c2e67af6dcc3218b1d755b59317c967723c
Jay200123 <kenken.ayonon@gmail.com>
2024-05-13 17:39:44 +0800

    Fix sms message

--- services/appointmentService.js
+++ services/appointmentService.js
@@ -15,18 +15,6 @@ const getAdminUsers = async () => {
   return admins;
 };
 
-const deleteAppointmentAfterTimeout = async (appointmentId, verification) => {
-  const appointment = await Appointment.findById(appointmentId);
-
-  if (appointment && verification && verification.confirm === false) {
-    await Promise.all([
-      Appointment.findByIdAndDelete(appointmentId).lean().exec(),
-      Transaction.deleteMany({ appointment: appointmentId }).lean().exec(),
-      Verification.deleteMany({ transaction: verification._id }).lean().exec(),
-    ]);
-  }
-};
-
 exports.getAllAppointmentsData = async () => {
   const appointments = await Appointment.find()
     .sort({ createdAt: STATUSCODE.NEGATIVE_ONE })
@@ -377,7 +365,7 @@ exports.updateScheduleAppointmentData = async (req, res, id) => {
     }
   ).populate(RESOURCE.CUSTOMER, "name contact_number");
 
-  const smsMessage = `Dear ${updatedScheduleAppointment.customer.name}, Your appointment has been updated. Please wait for the admin to review and confirm. Thank you for choosing Lhanlee Salon!`;
+  const smsMessage = `Dear ${updatedScheduleAppointment.customer.name}, Your appointment has been updated. Please wait for the admin to review and confirm.`;
 
   console.log(smsMessage);
 
@@ -476,7 +464,7 @@ exports.cancelRebooked = async (appointmentId) => {
       }
     );
 
-    const smsMessage = `Dear ${appointment.customer.name}, Your rebooking has been denied. Your appointment has been reverted to its original date and time. Thank you for your understading!`;
+    const smsMessage = `Dear ${appointment.customer.name}, Your rebooking has been denied. Your appointment has been reverted to its original date and time.thank you for understanding if you have any problems you can contact support`;
 
     console.log(smsMessage);
 
